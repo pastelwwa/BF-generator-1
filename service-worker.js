767c9ab34daf2fbf40f4822e7c4f43238b052c19
pastelwwa <pastel.wwa@gmail.com>
2025-10-11 01:12:19 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "bfgen-v7";
+const CACHE = "bfgen-v8";
 const ASSETS = [
   "./",
   "./index.html",
@@ -6,7 +6,6 @@ const ASSETS = [
   "./styles.css",
   "./app.js",
   "./manifest.webmanifest",
-  // obrazy i warstwy
   "./pliki/appicon.png",
   "./pliki/D_okno.png",
   "./pliki/M_okno.png",
@@ -16,7 +15,6 @@ const ASSETS = [
   "./pliki/M_ramka.png",
   "./pliki/M_nakladka.png",
   "./pliki/M_napis.png",
-  // fonty
   "./pliki/fonts/TT-Travels-Next-DemiBold.woff2",
   "./pliki/fonts/TT-Commons-Medium.woff2"
 ];
@@ -24,13 +22,11 @@ const ASSETS = [
 self.addEventListener("install", e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
 });
-
 self.addEventListener("activate", e=>{
   e.waitUntil(
     caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
   );
 });
-
 self.addEventListener("fetch", e=>{
   e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request)));
 });
